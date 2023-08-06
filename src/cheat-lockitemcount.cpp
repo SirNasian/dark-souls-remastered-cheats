419aa07014faa7f868a6b53dbd70ccde286d318f
Nathan Iszlaub <nathan.iszlaub@gmail.com>
2023-08-06 20:14:54 +0800

    Implement `LockItemCount::status` via `ptrace`

--- src/cheat-lockitemcount.cpp
+++ src/cheat-lockitemcount.cpp
@@ -1,8 +1,21 @@
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+
 #include "cheat-lockitemcount.hpp"
 
 bool LockItemCount::status(unsigned int pid)
 {
-	return true;
+	long long data;
+
+	if (ptrace(PTRACE_ATTACH, pid, 0, 0) == -1) return false;
+	waitpid(pid, (int*)(&data), 0);
+
+	data = ptrace(PTRACE_PEEKDATA, pid, (void*)0x14074f295, 0);
+	ptrace(PTRACE_DETACH, pid);
+
+	unsigned char* ptr = (unsigned char*)(&data);
+	return (ptr[0] != 0x03) ||
+	       (ptr[1] != 0xc5);
 }
 
 void LockItemCount::enable(unsigned int pid) {};
